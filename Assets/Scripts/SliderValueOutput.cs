01ef32bb38e0b5a0bb5b0f5a3ec0dd19b2f73997
Marco Duran <marco.duran456@gmail.com>
2018-01-13 02:22:16 -0500

    Created components that can check inputs in the input tester scene

---
+++ Assets/Scripts/SliderValueOutput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueOutput : MonoBehaviour {
+    public Text sliderVal;
+    public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		if(sliderVal != null)
+        {
+            sliderVal.text = slider.value.ToString();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sliderVal.text = slider.value.ToString();
+	}
+}
