61e7e509aab9bc1737cc9fd7afd581fe2ac48cf2
Marco Duran <marco.duran456@gmail.com>
2018-01-13 09:38:43 -0500

    Added base classes for controller

---
+++ Assets/Scripts/Tester/Tester.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum InputDevice
+{
+    button,
+    limitSwitch,
+    slider,
+};
+
+public class Tester : MonoBehaviour {
+    public byte opcode;
+    public byte input;
+    public string inputVal;
+    public string opcodeVal;
+    public Text textBox;
+    public InputDevice currDeviceType;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Getters
+
+    /**
+     *   Getting the opcode from the device
+     * 
+     * 
+     */
+     //public byte GetOpCode
+
+    //Setters
+
+    /**
+     * 
+     *  Setting the opcode for the device
+     * 
+     */
+}
